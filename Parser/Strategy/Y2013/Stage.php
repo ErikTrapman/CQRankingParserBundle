e160f9c8cda56658c176489bc203aa107bbb318e
ErikTrapman <veggatron@gmail.com>
2013-01-22 21:48:24 +0100

    inconsistent HTML on cqranking...

--- Parser/Strategy/Y2013/Stage.php
+++ Parser/Strategy/Y2013/Stage.php
@@ -6,12 +6,15 @@ use ErikTrapman\Bundle\CQRankingParserBundle\Parser\Strategy\AbstractStrategy;
 use ErikTrapman\Bundle\CQRankingParserBundle\Parser\Strategy\ParserStrategyInterface;
 use Symfony\Component\DomCrawler\Crawler;
 
-
 class Stage extends AbstractStrategy implements ParserStrategyInterface
 {
 
     public function parseResults(Crawler $crawler)
     {
-        return $this->parseResultsFromExpression($crawler, 'table.border tr');
+        $res = $this->parseResultsFromExpression($crawler, 'table.border tr');
+        if (!empty($res)) {
+            return $res;
+        }
+        return $this->parseResultsFromExpression($crawler, 'table.bordertop tr');
     }
 }
