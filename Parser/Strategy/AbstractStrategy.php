4248d44d4a8a8ac4f47a4737dad71073b8681ba6
Erik Trapman <veggatron@gmail.com>
2013-11-02 19:55:00 +0100

    proper license and copyright

--- Parser/Strategy/AbstractStrategy.php
+++ Parser/Strategy/AbstractStrategy.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the CQ-ranking parser package.
+ *
+ * (c) Erik Trapman <veggatron@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace ErikTrapman\Bundle\CQRankingParserBundle\Parser\Strategy;
 
 use ErikTrapman\Bundle\CQRankingParserBundle\Parser\Exception\CQParserException;
--- Tests/CQRanking/Parser/Match/MatchParserTest.php
+++ Tests/CQRanking/Parser/Match/MatchParserTest.php
@@ -1,4 +1,13 @@
 <?php
+
+/*
+ * This file is part of the CQ-ranking parser package.
+ *
+ * (c) Erik Trapman <veggatron@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace ErikTrapman\Bundle\CQRankingParserBundle\Tests\CQRanking\Parser\Match;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
