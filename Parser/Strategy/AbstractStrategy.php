e83baa8aa55252746239e05b1adebcff14ddbb80
Erik Trapman <veggatron@gmail.com>
2015-05-16 13:27:08 +0200

    display strategyclass

--- Parser/Strategy/AbstractStrategy.php
+++ Parser/Strategy/AbstractStrategy.php
@@ -20,11 +20,16 @@ abstract class AbstractStrategy implements ParserStrategyInterface
 
     public function __construct()
     {
-        
+
+    }
+
+    public function __toString()
+    {
+        return get_class($this);
     }
 
     /**
-     * 
+     *
      * @param Crawler $crawler
      * @param type $url
      * @param type $expr
@@ -33,41 +38,41 @@ abstract class AbstractStrategy implements ParserStrategyInterface
      */
     protected function parseResultsFromExpression(Crawler $crawler, $expr)
     {
-        $data = $crawler->filter($expr)->filter('tr')->each(function($node, $i) {
-                $returnValues = array();
-                foreach ($node->filter('td') as $key => $currentResult) {
-                    if ($key == 1) {
-                        $pos = trim($currentResult->nodeValue, '.');
-                        if (0 !== strcmp('leader', $pos) && !is_numeric($pos)) {
-                            break;
-                        }
-                        $returnValues['pos'] = $pos;
+        $data = $crawler->filter($expr)->filter('tr')->each(function ($node, $i) {
+            $returnValues = array();
+            foreach ($node->filter('td') as $key => $currentResult) {
+                if ($key == 1) {
+                    $pos = trim($currentResult->nodeValue, '.');
+                    if (0 !== strcmp('leader', $pos) && !is_numeric($pos)) {
+                        break;
                     }
-                    if ($key == 3) {
-                        $img = $currentResult->getElementsByTagName("img");
-                        if ($img->length) {
-                            $gif = $img->item(0)->getAttribute('src');
-                            $parts = explode(".", basename($gif));
-                            $returnValues['nat'] = $parts[0];
-                        } else {
-                            $returnValues['nat'] = null;
-                        }
-                    }
-                    if ($key == 5) {
-                        $returnValues['name'] = $currentResult->nodeValue;
-                        $hyperlink = $currentResult->getElementsByTagName('a');
-                        $riderHref = $hyperlink->item(0)->getAttribute('href');
-
-                        $riderId = substr($riderHref, strpos($riderHref, '=') + 1);
-                        $returnValues['cqranking_id'] = $riderId;
-                    }
-                    if ($key == 11) {
-                        $returnValues['points'] = $currentResult->nodeValue;
+                    $returnValues['pos'] = $pos;
+                }
+                if ($key == 3) {
+                    $img = $currentResult->getElementsByTagName("img");
+                    if ($img->length) {
+                        $gif = $img->item(0)->getAttribute('src');
+                        $parts = explode(".", basename($gif));
+                        $returnValues['nat'] = $parts[0];
+                    } else {
+                        $returnValues['nat'] = null;
                     }
                 }
-                return $returnValues;
-            });
-            //array_values to generate new keys, starting from 0
+                if ($key == 5) {
+                    $returnValues['name'] = $currentResult->nodeValue;
+                    $hyperlink = $currentResult->getElementsByTagName('a');
+                    $riderHref = $hyperlink->item(0)->getAttribute('href');
+
+                    $riderId = substr($riderHref, strpos($riderHref, '=') + 1);
+                    $returnValues['cqranking_id'] = $riderId;
+                }
+                if ($key == 11) {
+                    $returnValues['points'] = $currentResult->nodeValue;
+                }
+            }
+            return $returnValues;
+        });
+        //array_values to generate new keys, starting from 0
         return array_values(array_filter($data, create_function('$a', 'return !empty($a);')));
     }
 }
